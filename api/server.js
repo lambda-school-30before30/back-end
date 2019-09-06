0cc166db49ebc1feb09492364cf25fdfaea47dbc
schang1146 <sammychang2185@gmail.com>
2019-09-05 22:55:42 -0700

    able to login and logout

--- api/server.js
+++ api/server.js
@@ -1,34 +1,57 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const express = require('express');
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+const cors = require('cors');
+const helmet = require('helmet');
+const secret = require('../config/secret.js');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
-const db = require("../database/dbConfig");
+
+const sessionOptions = {
+    name: '30before30_login',
+    secret: secret.jwtSecret,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 2, // 2hr
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+
+    store: new knexSessionStore({
+        knex: require('../database/dbConfig.js'),
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        createtable: true,
+        clearInterval: 1000 * 60 * 60 * 2
+    })
+};
+
+const db = require('../database/dbConfig');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionOptions));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
-server.get("/", (req, res) => {
-  res.send(`<h1>BUILD WEEK BAYBAY</h1>`);
+server.get('/', (req, res) => {
+    res.send(`<h1>BUILD WEEK BAYBAY</h1>`);
 });
 
-server.get("/test", async (req, res) => {
-  const users = await db("users");
+server.get('/test', async (req, res) => {
+    const users = await db('users');
 
-  try {
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const secret = require('../config/secret.js');
 
 const Users = require('../users/users-model');
 
@@ -15,7 +16,59 @@ router.post('/register', (req, res) => {
         })
         .catch(err => {
             res.status(500).json(err);
-        })
+        });
 });
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    Users.findBy({ username })
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = genToken(user);
+
+                req.session.token = token;
+
+                res.status(200).json({
+                    message: `Logged in as ${username}`,
+                    token
+                });
+            } else {
+                res.status(401).json({
+                    message: `Error: Invalid credentials!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/logout', (req, res) => {
+    if (req.session.token) {
+        req.session.destroy(err => {
+            if (err) {
+                res.status(400).send('Error: Unable to logout');
+            } else {
+                res.send('Logged out!');
+            }
+        });
+    } else {
+        res.end();
+    }
+});
+
+function genToken(user) {
+    const payload = {
+        subject: 'username',
+        username: user.username
+    };
+
+    const options = {
+        expiresIn: '2h'
+    };
+
+    return jwt.sign(payload, secret.jwtSecret, options);
+}
 
 module.exports = router;
