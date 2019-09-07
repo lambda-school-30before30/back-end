71f567b106edd0fb9cddbf3faaa452958060ce83
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-09-06 22:55:46 -0400

    added another test for activities/:id

--- activities/activity-router.spec.js
+++ activities/activity-router.spec.js
@@ -3,6 +3,10 @@ const server = require('../api/server');
 const db = require('../database/dbConfig');
 
 describe('activity-router.js', () => {
+
+    beforeEach(async () => {
+        await db('activities').truncate();
+    });
     
     describe('GET /api/activities', () => {
         it('should return a status of 200', async () => {
@@ -10,5 +14,14 @@ describe('activity-router.js', () => {
             const res = await request(server).get('/');
             expect(res.status).toEqual(expectedStatus);
         })
-    })
+    });
+
+    describe('GET /api/activities/:id', () => {
+        it('should return the activity with that id', async () => {
+            const expectedStatus = 200;
+            const res = await request(server).get('/:id');
+            expect(res.status).toEqual(expectedStatus);
+        });
+    });
+    
 });
