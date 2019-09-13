0dd030e8824bea7ff556c280d1e81022e6f75cbd
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-09-12 22:52:27 -0400

    all tests pass

--- activities/activity-router.spec.js
+++ activities/activity-router.spec.js
@@ -7,23 +7,53 @@ describe('activity-router.js', () => {
     beforeEach(async () => {
         await db('activities').truncate();
     });
-    
-    describe('GET /api/activities', () => {
-        it('should return a 200 status', async () => {
-            const test = await request(server).post('/api/auth/login').send({
+    let token;
+
+    beforeAll((done) => {
+        request(server)
+            .post('/api/auth/login')
+            .send({
                 username: 'testUser',
                 password: 'password123'
             })
-            const expectedStatus = 200;
-            // const res = await request(server).get('/api/activities');
-            // expect(res.status).toEqual(expectedStatus);
-            // console.log(test);
-            request(server).get('/api/activities').set({authorization: test})
-                .then(res => {
-                    expect(res.status).toEqual(expectedStatus);
-                })
-        })        
+            .end((err, response) => {
+                token = response.body.token;
+                done();
+            });
     });
+
+    describe('GET /api/activities', () => {
+        it('should require authorization', () => {
+            return request(server)
+                .get('/api/activities')
+                .then((response) => {
+                    expect(response.status).toEqual(401);
+                });
+        });
+        it('responds with JSON', () => {
+            return request(server)
+                .get('/api/activities')
+                .set('Authorization', `Bearer ${token}`)
+                .then((response) => {
+                    // expect(response.status).toEqual(200);
+                    expect(response.type).toEqual('application/json');
+                });
+        });
+    });
+    
+    // describe('GET /api/activities', () => {
+    //     it('should return a 200 status', async () => {
+    //         const test = await request(server).post('/api/auth/login').send({
+    //             username: 'testUser',
+    //             password: 'password123'
+    //         })
+    //         request(server).get('/api/activities')
+    //             .set({authorization: test})
+    //             .then(res => {
+    //                 expect(res.status).toEqual(200);
+    //             })
+    //     })        
+    // });
     
     describe('GET /api/activities/public', () => {
         it('should return 200 status', async () => {
@@ -33,10 +63,9 @@ describe('activity-router.js', () => {
                 isCompleted: false,
                 user_id: 2
             })
-            const expectedStatus = 200;
-            // const res = await activitiesHelper.find().where({isPublic: true});
+            
             const res = await request(server).get('/api/activities/public')
-            expect(res.status).toEqual(expectedStatus);
+            expect(res.status).toEqual(200);
         })        
     });
 
@@ -52,5 +81,52 @@ describe('activity-router.js', () => {
             const res = await request(server).get('/api/activities/:id')
             expect(res.status).toEqual(200);
         })
-    })
+    });
+
+    describe('POST /api/activities', () => {
+        it('should return 201', async () => {
+            let activity = {
+                title: 'node build week',
+                isPublic: false,
+                isCompleted: false,
+                user_id: 1
+            };
+
+            request(server)
+                .post('/api/activities')
+                .send(activity)
+                .expect(201)
+        });
+    });
+
+    describe('PUT /api/activities/:id', () => {
+        it('should update activity with that id', async () => {
+            let activity = {
+                title: 'updated node build week',
+                isPublic: false,
+                isCompleted: false,
+                user_id: 1
+            };
+
+            request(server)
+                .put('/api/activities/:id')
+                .send(activity)
+                .expect(204)
+        });
+    });
+
+    describe('DELETE /api/activities/:id', () => {
+        it('should delete the activity with that id', async () => {
+            let activity = {
+                title: 'node build week',
+                isPublic: false,
+                isCompleted: false,
+                user_id: 1
+            };
+
+            request(server)
+                .delete('/api/activities/:id')
+                .expect(200)
+        });
+    });
 });
