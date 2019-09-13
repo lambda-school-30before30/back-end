2991c46478ca5e631992b89a4605e3c117f5158d
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-09-12 21:03:49 -0400

    working on tests for authenticated routes

--- activities/activity-router.spec.js
+++ activities/activity-router.spec.js
@@ -1,23 +1,56 @@
 const request = require('supertest');
 const server = require('../api/server');
 const db = require('../database/dbConfig');
+const activitiesHelper = require('../activities/activity-model');
 
 describe('activity-router.js', () => {
+    beforeEach(async () => {
+        await db('activities').truncate();
+    });
     
     describe('GET /api/activities', () => {
-        it('should return a status of 200', async () => {
+        it('should return a 200 status', async () => {
+            const test = await request(server).post('/api/auth/login').send({
+                username: 'testUser',
+                password: 'password123'
+            })
             const expectedStatus = 200;
-            const res = await request(server).get('/');
+            // const res = await request(server).get('/api/activities');
+            // expect(res.status).toEqual(expectedStatus);
+            // console.log(test);
+            request(server).get('/api/activities').set({authorization: test})
+                .then(res => {
+                    expect(res.status).toEqual(expectedStatus);
+                })
+        })        
+    });
+    
+    describe('GET /api/activities/public', () => {
+        it('should return 200 status', async () => {
+            await activitiesHelper.add({
+                title: "test title",
+                isPublic: true,
+                isCompleted: false,
+                user_id: 2
+            })
+            const expectedStatus = 200;
+            // const res = await activitiesHelper.find().where({isPublic: true});
+            const res = await request(server).get('/api/activities/public')
             expect(res.status).toEqual(expectedStatus);
         })        
     });
 
     describe('GET /api/activities/:id', () => {
-        it('should return the activity with that id', async () => {
-
-            const res = await request(server).get('/:id');
+        it('should return a single activity with given id', async () => {
+            
+            await activitiesHelper.add({
+                title: "new activity",
+                isPublic: true,
+                isCompleted: false,
+                user_id: 1
+            })
+            const res = await request(server).get('/api/activities/:id')
             expect(res.status).toEqual(200);
-        });
-    });
-    
+        })
+    })
 });
