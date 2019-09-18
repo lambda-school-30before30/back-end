c665edc4c80c745d7d19441d689564156a2f1082
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-09-18 01:23:39 -0400

    still deploying

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,17 @@
 // Update with your config settings.
 
 module.exports = {
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
   development: {
     client: "sqlite3",
     useNullAsDefault: true,
