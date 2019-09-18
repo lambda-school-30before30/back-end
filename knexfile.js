ad0076095bbdcf74db14b9a57e625c22cd576446
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-09-18 01:41:41 -0400

    almost deployed and working

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./database/migrations"
