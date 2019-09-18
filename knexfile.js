229827b2a1530d9f45b1e0864e9ad93188fb7e4d
schang1146 <sammychang2185@gmail.com>
2019-09-17 23:02:57 -0700

    dead

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: process.env["postgres://utbokzicloyckp:c7ad3429a995a674d88a89daae16af98775d27dc055aa9d03b491b7995f6c90f@ec2-174-129-227-80.compute-1.amazonaws.com:5432/da1b8kecu16b9e"],
+      database: "postgres://utbokzicloyckp:c7ad3429a995a674d88a89daae16af98775d27dc055aa9d03b491b7995f6c90f@ec2-174-129-227-80.compute-1.amazonaws.com:5432/da1b8kecu16b9e",
     },
     migrations: {
       directory: "./database/migrations"
