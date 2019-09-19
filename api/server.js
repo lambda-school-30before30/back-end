ca8b918a08a23587cf3f3bb4f20dadbda8397ea6
Braden Mason <bradenemason@gmail.com>
2019-09-18 19:42:06 -0500

    added cors to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const session = require('express-session');
-const knexSessionStore = require('connect-session-knex')(session);
-const cors = require('cors');
-const helmet = require('helmet');
-const secret = require('../config/secret.js');
+const express = require("express");
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
+const cors = require("cors");
+const helmet = require("helmet");
+const secret = require("../config/secret.js");
 
 const activityRouter = require("../activities/activity-router");
 const authRouter = require("../auth/auth-router.js");
@@ -13,29 +13,30 @@ const commentsRouter = require("../comments/comments.js");
 const server = express();
 
 const sessionOptions = {
-    name: '30before30_login',
-    secret: secret.jwtSecret,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 2, // 2hr
-        secure: false,
-        httpOnly: true
-    },
-    resave: false,
-    saveUninitialized: false,
+  name: "30before30_login",
+  secret: secret.jwtSecret,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 2, // 2hr
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
 
-    store: new knexSessionStore({
-        knex: require('../database/dbConfig.js'),
-        tablename: 'sessions',
-        sidfieldname: 'sid',
-        createtable: true,
-        clearInterval: 1000 * 60 * 60 * 2
-    })
+  store: new knexSessionStore({
+    knex: require("../database/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60 * 2
+  })
 };
 
-const db = require('../database/dbConfig');
+const db = require("../database/dbConfig");
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 server.use(session(sessionOptions));
 
 server.use("/api/activities", activityRouter);
@@ -43,8 +44,8 @@ server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/activities", commentsRouter);
 
-server.get('/', (req, res) => {
-    res.send(`<h1>BUILD WEEK BAYBAY</h1>`);
+server.get("/", (req, res) => {
+  res.send(`<h1>BUILD WEEK BAYBAY</h1>`);
 });
 
 module.exports = server;
